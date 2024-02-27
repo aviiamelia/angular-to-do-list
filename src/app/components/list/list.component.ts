70d0cb1bd4ea55bfe36537da4e47865afab925ab
rafael <rafael.zoe26@gmail.com>
2024-02-27 02:12:36 -0300

    feat: integrating list component and add types

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -8,6 +8,10 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
   @Input() isChecked: boolean = false;
   @Input() itemName: string = '';
+  @Output() checkboxChanged = new EventEmitter();
 
+  onCheckboxChange() {
+    this.checkboxChanged.emit();
+  }
   ngOnInit(): void {}
 }
