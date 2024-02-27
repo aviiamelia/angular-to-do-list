34e0eec8041616b39f28e9c4a4111e6c18107156
rafael <rafael.zoe26@gmail.com>
2024-02-27 00:39:53 -0300

    fix: project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  todoList = ['lavar roupa', 'ir ao mercado', 'assistir série'];
   title = 'to-do-list';
 }
